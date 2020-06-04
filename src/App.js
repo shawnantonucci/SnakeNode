f8547dc8d450ef89b24a19dc7226d99ce3c1cc32
Shawn Antonucci <shawnantonucci2018@gmail.com>
2020-06-04 01:02:26 -0400

    snake mechanics with gameover for boundary hit and snake eaten

--- src/App.js
+++ src/App.js
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import { useInterval } from "./useInterval";
+import {
+  CANVAS_SIZE,
+  SNAKE_START,
+  APPLE_START,
+  SCALE,
+  SPEED,
+  DIRECTIONS,
+} from "./constants";
 
-function App() {
+const App = () => {
+  const canvasRef = useRef(null);
+  const [snake, setSnake] = useState(SNAKE_START);
+  const [apple, setApple] = useState(APPLE_START);
+  const [direction, setDirection] = useState([0, -1]);
+  const [speed, setSpeed] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+  const [boundaryHit, setBoundaryHit] = useState(false);
+  const [message, setMessage] = useState(null);
+
+  const startGame = () => {
+    setSnake(SNAKE_START);
+    setApple(APPLE_START);
+    setDirection([0, -1]);
+    setSpeed(SPEED);
+    setGameOver(false);
+  };
+
+  const endGame = () => {
+    setSpeed(null);
+    setGameOver(true);
+  };
+
+  const moveSnake = ({ keyCode }) => {
+    keyCode >= 37 && keyCode <= 40 && setDirection(DIRECTIONS[keyCode]);
+  };
+
+  const spawnApple = () =>
+    apple.map((_a, i) => Math.floor(Math.random() * (CANVAS_SIZE[i] / SCALE)));
+
+  const checkCollision = (head, snk = snake) => {
+    if (
+      head[0] * SCALE >= CANVAS_SIZE[0] ||
+      head[0] < 0 ||
+      head[1] * SCALE >= CANVAS_SIZE[1] ||
+      head[1] < 0
+    ) {
+      setBoundaryHit(true);
+      return true;
+    }
+    for (const segment of snk) {
+      if (head[0] === segment[0] && head[1] === segment[1]) {
+        setBoundaryHit(false);
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const checkAppleCollision = (newSnake) => {
+    if (newSnake[0][0] === apple[0] && newSnake[0][1] === apple[1]) {
+      let newApple = spawnApple();
+      while (checkCollision(newApple, newSnake)) {
+        newApple = spawnApple();
+      }
+      setApple(newApple);
+      return true;
+    }
+    return false;
+  };
+
+  const gameLoop = () => {
+    const snakeCopy = JSON.parse(JSON.stringify(snake));
+    const newSnakeHead = [
+      snakeCopy[0][0] + direction[0],
+      snakeCopy[0][1] + direction[1],
+    ];
+    snakeCopy.unshift(newSnakeHead);
+    if (checkCollision(newSnakeHead)) {
+      endGame();
+    }
+    if (!checkAppleCollision(snakeCopy)) {
+      snakeCopy.pop();
+    }
+    setSnake(snakeCopy);
+  };
+
+  useEffect(() => {
+    const context = canvasRef.current.getContext("2d");
+    context.setTransform(SCALE, 0, 0, SCALE, 0, 0);
+    context.clearRect(0, 0, CANVAS_SIZE[0], CANVAS_SIZE[1]);
+    context.fillStyle = "green";
+    context.shadowColor = "black";
+    context.shadowBlur = 1;
+    snake.forEach(([x, y]) => context.fillRect(x, y, 1, 1));
+    context.fillStyle = "red";
+    context.fillRect(apple[0], apple[1], 1, 1);
+  }, [snake, apple, gameOver]);
+
+  useEffect(() => {
+    if (boundaryHit) {
+      setMessage("Hit the end of the world");
+    } else {
+      setMessage("You ate yourself");
+    }
+  }, [boundaryHit]);
+
+  useInterval(() => gameLoop(), speed);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div role="button" tabIndex="0" onKeyDown={(e) => moveSnake(e)}>
+      <canvas
+        style={{ border: "1px solid black" }}
+        ref={canvasRef}
+        width={`${CANVAS_SIZE[0]}px`}
+        height={`${CANVAS_SIZE[1]}px`}
+      />
+      {gameOver && <div>{message}... Game Over</div>}
+      <button onClick={startGame}>Start Game</button>
     </div>
   );
-}
+};
 
 export default App;
